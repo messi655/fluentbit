e5ef76695a75322d2792890c6eb8114fccee5eab
vantinhuynh <tin@trustingsocial.com>
2019-04-14 22:16:08 +0700

    update tags

--- install-fluent-bit.sh
+++ install-fluent-bit.sh
@@ -10,12 +10,12 @@ apt-get update -y
 
 apt-get install td-agent-bit -y
 
-cat /etc/environment|grep ELASTICSEARCH_HOST
-return_code=$(echo $?)
-if [ $return_code == 1 ];then
-        echo "ELASTICSEARCH_HOST="es.internal.cloudhms.io"" >> /etc/environment
-fi
-source /etc/environment
+# Change Elasticsearch Host and Port
+ELASTICSEARCH_HOST="es.host.com"
+ELASTICSEARCH_PORT="80"
+
+# PROD_NAME is product name, This will be create a index on Elasticsearch with the name is product name, separate the product name.
+PROD_NAME="the_name"
 
 rm -f /etc/td-agent-bit/td-agent-bit.conf
 rm -f /etc/td-agent-bit/parsers.conf
@@ -36,10 +36,29 @@ wget https://raw.githubusercontent.com/messi655/fluentbit/master/config/filter-a
 wget https://raw.githubusercontent.com/messi655/fluentbit/master/config/filter-apache-access.conf
 wget https://raw.githubusercontent.com/messi655/fluentbit/master/config/plugins.conf
 
+# Replace Elasticsearch host
 sed -i 's/elasticsearch_host/'"$ELASTICSEARCH_HOST"'/g' output-elasticsearch-apache-access.conf 
 sed -i 's/elasticsearch_host/'"$ELASTICSEARCH_HOST"'/g' output-elasticsearch-php-fpm.conf 
 sed -i 's/elasticsearch_host/'"$ELASTICSEARCH_HOST"'/g' output-elasticsearch-apache-error.conf 
 
+# Replace Elasticsearch port
+sed -i 's/elasticsearch_port/'"$ELASTICSEARCH_PORT"'/g' output-elasticsearch-apache-access.conf 
+sed -i 's/elasticsearch_port/'"$ELASTICSEARCH_PORT"'/g' output-elasticsearch-php-fpm.conf 
+sed -i 's/elasticsearch_port/'"$ELASTICSEARCH_PORT"'/g' output-elasticsearch-apache-error.conf 
+
+# Replace prod_name for Index on ES
+sed -i 's/prod_name/'"$PROD_NAME"'/g' output-elasticsearch-apache-access.conf 
+sed -i 's/prod_name/'"$PROD_NAME"'/g' output-elasticsearch-php-fpm.conf 
+sed -i 's/prod_name/'"$PROD_NAME"'/g' output-elasticsearch-apache-error.conf 
+
+# Replace prod_name for filter
+sed -i 's/prod_name/'"$PROD_NAME"'/g' filter-apache-access.conf 
+sed -i 's/prod_name/'"$PROD_NAME"'/g' filter-apache-error.conf 
+sed -i 's/prod_name/'"$PROD_NAME"'/g' filter-php-fpm.conf 
+
+
 systemctl enable td-agent-bit
 
 systemctl start td-agent-bit
+
+
